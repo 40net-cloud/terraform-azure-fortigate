085030dd1b9c19ddb60408a459517e93e3c00069
Joeri <jvanhoof@fortinet.com>
2025-07-17 11:53:49 +0200

    Update variables.tf

--- modules/azurevirtualwan/variables.tf
+++ modules/azurevirtualwan/variables.tf
@@ -116,7 +116,7 @@ variable "plan" {
     name      = "fortigate-managedvwan"
     product   = "fortigate_vwan_nva"
     publisher = "fortinet"
-    version   = "7.4.700250619"
+    version   = "7.4.800250704"
   }
 }
 
