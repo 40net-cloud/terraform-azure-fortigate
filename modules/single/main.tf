201709f0da700c7d06ccdf46d95e4092a3375e91
Joeri Van Hoof <jvanhoof@fortinet.com>
2026-06-04 00:03:39 +0200

    Disable default outbound capability and add outbound rule for LB

--- modules/single/main.tf
+++ modules/single/main.tf
@@ -80,21 +80,21 @@ resource "azurerm_linux_virtual_machine" "fgtvm" {
 
   source_image_reference {
     publisher = "fortinet"
-    offer     = "fortinet_fortigate-vm_v5"
+    offer     = var.fgt_image_offer
     sku       = var.fgt_image_sku
     version   = var.fgt_version
   ***REMOVED***
 
   plan {
     publisher = "fortinet"
-    product   = "fortinet_fortigate-vm_v5"
+    product   = var.fgt_image_offer
     name      = var.fgt_image_sku
   ***REMOVED***
 
   os_disk {
     name                 = "${local.fgt_name***REMOVED***-osdisk"
     caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    storage_account_type = "Premium_LRS"
   ***REMOVED***
 
   admin_username                  = var.username
@@ -125,7 +125,7 @@ resource "azurerm_managed_disk" "fgtvm-datadisk" {
   name                 = "${local.fgt_name***REMOVED***-datadisk-${count.index***REMOVED***"
   location             = var.location
   resource_group_name  = var.resource_group_name
-  storage_account_type = "Standard_LRS"
+  storage_account_type = var.fgt_datadisk_storage_account_type
   create_option        = "Empty"
   disk_size_gb         = var.fgt_datadisk_size
 ***REMOVED***
